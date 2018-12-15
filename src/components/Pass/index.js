dbb9a51fcae5f23181cdaac768d171098197b9f9
Wiput Pootong <wiput.pootong@gmail.com>
2018-12-15 16:13:55 +0700

    :sparkles: add fetch api

--- src/components/Pass/index.js
+++ src/components/Pass/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import QRCode from 'qrcode.react'
 
 import Card from './Card'
@@ -6,12 +7,32 @@ import Content from './Content'
 import Text from './Text'
 import Wrapper from './Wrapper'
 
+const api = axios.create({
+  baseURL: 'https://actions-on-falcon.herokuapp.com'
+})
+
 const Pass = () => {
+  const [pass, setPass] = useState({})
+
+  useEffect(() => {
+    getCode()
+  })
+
+  const getCode = async () => {
+    const id = 'nlbvdu'
+    const {data: code} = await api.get(`/pass/${id}`)
+    if (!code) return
+
+    setPass(code)
+  }
+
+  const {name = '', code = '', time = '', phone = ''} = pass
+
   return (
     <Card>
       <Content>
         <Wrapper>
-          <QRCode className="qr" value="http://facebook.github.io/react/" />
+          <QRCode className="qr" value={code} />
         </Wrapper>
         <Text>Visitor Pass</Text>
       </Content>
