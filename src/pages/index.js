859ef2d3952cb6515b3ef4f10945275998a91360
Pmc Dev <phoomparin@gmail.com>
2018-12-15 14:07:30 +0700

    :memo: Update gatsby config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
+import Image from '../components/image'
+import Layout from '../components/layout'
+
+const keywords = ['gatsby', 'application', 'react']
 
 const IndexPage = () => (
   <Layout>
-    <SEO title='Home' keywords={['gatsby', 'application', 'react']} />
+    <SEO title="Home" keywords={keywords} />
+
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+
+    <div style={{maxWidth: '300px', marginBottom: '1.45rem'}}>
       <Image />
     </div>
   </Layout>
