ea6fc1cc19286aef0379efb53400c70e2ed95cad
Wiput Pootong <wiput.pootong@gmail.com>
2018-12-16 12:13:33 +0700

    :sparkles: add QR border

--- src/components/Pass/Wrapper.js
+++ src/components/Pass/Wrapper.js
@@ -2,7 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-  padding: 10px 10px 0 10px;
+  margin: 10px 10px 0 10px;
+  border: #fff solid 5px;
+  background-color: #fff;
 `
 
 export default Wrapper
