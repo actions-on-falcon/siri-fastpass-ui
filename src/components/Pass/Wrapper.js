f16dcaba9c8b5bc313b0fd59a91bf156caa4333c
Pmc Dev <phoomparin@gmail.com>
2018-12-16 12:18:56 +0700

    :sparkles: Use white background for QR

--- src/components/Pass/Wrapper.js
+++ src/components/Pass/Wrapper.js
@@ -2,9 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-  margin: 10px 10px 0 10px;
-  border: #fff solid 5px;
-  background-color: #fff;
+  background: white;
+  padding: 17px;
+  box-shadow: 10px 10px 45px -10px rgba(0, 0, 0, 0.15);
 `
 
 export default Wrapper
